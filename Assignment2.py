fa05f4201cb461c6e8a38cd6f7d143bc1490df1e
nobleMartinz <martinchiemekaeze@gmail.com>
2026-07-01 15:48:10 +0100

    first commit

---
+++ Assignment2.py
@@ -0,0 +1,18 @@
+"""
+Assignment 2: Addition of Two Numbers
+Write a Python program that:
+
+Takes two numbers from the user.
+Converts both inputs into integers using int().
+Finds the sum of the two numbers.
+Stores the result in a variable called sum.
+Displays the result using the print() function.
+"""
+
+num1=input("please enter your first number")
+num1=int(num1)
+num2=input("please enter your second number")
+num2=int(num2)
+sum=num1+num2
+
+print("the result is:-",sum)
---
+++ Assignment3.py
@@ -0,0 +1,18 @@
+"""
+Assignment 3: Difference of Two Numbers
+Write a Python program that:
+
+Takes two numbers from the user.
+Converts both inputs into integers.
+Finds the difference between the two numbers.
+Stores the result in a variable called difference.
+Displays the result using the print() function.
+"""
+
+num1=input("please enter your first number")
+num1=int(num1)
+num2=input("please enter your second number")
+num2=int(num2)
+diff=num1-num2
+
+print("the result is:-",diff)
