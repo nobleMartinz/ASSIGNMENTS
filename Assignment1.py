297cb89a5a7db45ef9b19abd5d4f627d4c6ff37b
nobleMartinz <martinchiemekaeze@gmail.com>
2026-07-01 18:01:44 +0100

    first badge of the assignment completed

---
+++ Assignment1.py
@@ -0,0 +1,15 @@
+"""
+Assignment 1: Personal Introduction
+Write a Python program that:
+
+Takes your name as input from the user.
+Stores the name in a variable called name.
+Prints the message:
+
+"""
+
+#step 1: Take name as input and store it in a variable called name
+name=input("please enter your name")
+
+#step 2: print the message
+print(name)
